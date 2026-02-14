52440ca9447b35fd9757a415144c20d30c8a8585
周诗贺 (Shihe Zhou) <147121249+cran123@users.noreply.github.com>
2026-02-14 21:42:45 +0800

    Update train_fnfm.sh

--- train_fnfm.sh
+++ train_fnfm.sh
@@ -1,2 +1,2 @@
 cd FNFM-main/FNFM
-CUDA_VISIBLE_DEVICES=0 python 1Dmain_torchcfm.py --expIndex 1 --basemodel v_GWN --targetDataset collab --mode AE_CFM --denoise='cfmTransformer' --train_objective='cfm' --ae_arch layer_transformer --cfm_epochs 3000 --ae_epochs 1000
+CUDA_VISIBLE_DEVICES=0 python 1Dmain_torchcfm.py --expIndex 1 --basemodel v_STGCN5 --targetDataset collab --mode AE_CFM --denoise='cfmTransformer' --train_objective='cfm' --ae_arch layer_transformer --cfm_epochs 3000 --ae_epochs 1000
