b51f610980972b7480e7dc9cd26156b1395bc77a
周诗贺 (Shihe Zhou) <147121249+cran123@users.noreply.github.com>
2026-02-19 23:04:49 +0800

    Update train_stgcn_for_each.sh

--- train_stgcn_for_each.sh
+++ train_stgcn_for_each.sh
@@ -1,2 +1,2 @@
 cd FNFM-main/Pretrain
-CUDA_VISIBLE_DEVICES=5 python main_disturbe.py --graph_range ALL --model v_STGCN5 --dataname collab --epochs 25
+CUDA_VISIBLE_DEVICES=5 python main_disturbe.py --graph_range ALL --model v_STGCN5 --dataname collab --epochs 5
